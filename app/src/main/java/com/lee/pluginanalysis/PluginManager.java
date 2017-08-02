28565d234dca3943c81fcf7bb3723d8ff67d06f5
Lee <lee_ok168@sina.com>
2017-08-02 14:42:40 +0800

    add project

---
+++ app/src/main/java/com/lee/pluginanalysis/PluginManager.java
@@ -0,0 +1,16 @@
+package com.lee.pluginanalysis;
+
+import java.io.File;
+
+/**
+ * Description：
+ * Author：Lee
+ * Date：2017/8/1 15:40
+ */
+
+public class PluginManager {
+
+    public void installPlugin(File file) {
+
+    }
+}
